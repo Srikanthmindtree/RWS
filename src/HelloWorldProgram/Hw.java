93fd211afc9c8e480bab5aa9fab8720907b8600e
Srikanthmindtree <Srikanth.Sabhapathi@mindtree.com>
2022-08-29 14:32:56 +0000

    commit

---
+++ src/HelloWorldProgram/Hw.java
@@ -0,0 +1,12 @@
+package HelloWorldProgram;
+
+public class Hw {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.println("i am writing the code on remote windows desktop ");
+
+	}
+
+}
